f88594e5e645fc9ab14a07e9bf99d5f2f6dc6ece
wei41 <wei41@avaya.com>
2018-10-31 16:36:02 +0800

    B07-auth-protect-routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose')
 const multer =  require('multer');
 
+
 const Product = require('../models/product');
+const checkAuth = require('../middleware/check-auth')
 
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
@@ -103,7 +105,7 @@ router.get('/:productId',(req, res, next) => {
     })    
 })
 
-router.patch('/:productId',(req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {}
     for (const ops of req.body) {
@@ -130,7 +132,7 @@ router.patch('/:productId',(req, res, next) => {
     
 })
 
-router.delete('/:productId',(req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     Product.remove({
         _id: id
@@ -158,7 +160,7 @@ router.delete('/:productId',(req, res, next) => {
     })
 })
 
-router.post("/", upload.single('productImage'), (req, res, next) => {
+router.post("/", checkAuth, upload.single('productImage'), (req, res, next) => {
 
     console.log(req.file)
     const product = new Product({
