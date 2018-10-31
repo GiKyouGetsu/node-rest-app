019c39d5809e0462a61e3dcf6c7a5b90bf35aed5
wei41 <wei41@avaya.com>
2018-10-31 15:47:56 +0800

    B06-auth-login inital commit

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,8 @@
-const express  = require('express')
+const express  = require('express');
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -61,4 +62,45 @@ router.post("/signup", (req, res, next) => {
       });
   });
   
+  router.post('/login', (req, res, next) => {
+      User.find({ email: req.body.email })
+      .exec()
+      .then(user => {
+          if (user.length < 1) {
+              return res.status(401).json({
+                  message: 'Auth failed'
+              })
+          }
+          bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+              if (err) {
+                  return res.status(401).json({
+                      message: 'Auth Failed'
+                  })
+              }
+              if (result) {
+
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                },
+                process.env.JWT_KEY,
+                {
+                    expiresIn: '1h'
+                })
+                return res.status(200).json({
+                    message: 'Auth Successful',
+                    token: token
+                })    
+              }
+
+              res.status(401).json({
+                  message: 'Auth Failed'
+              })
+          })
+      })
+      .catch()
+  })
+
+
+
   module.exports = router;
